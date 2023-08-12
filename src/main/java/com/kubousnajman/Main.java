6949bfe127dc9e1ebd9062e71cc293fd2246bbf9
Jakub Najman <jakub@najman.cz>
2023-08-12 20:56:42 +0200

    read and create customer API

--- src/main/java/com/kubousnajman/Main.java
+++ src/main/java/com/kubousnajman/Main.java
@@ -2,30 +2,38 @@ package com.kubousnajman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main  {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse("hello, stranger!",
-                List.of("Java", "C++", "Javascript"),
-                new Person("Kubous", 22, 144000)
-        );
-        return response;
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
-    record Person (String name, int age, double money){}
-    record GreetResponse(String greet,
-                         List<String> faveProgrammingLanguages,
-                         Person person
-    ){}
+    record NewCustomerRequest (String name, String email, Integer age){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
 }
